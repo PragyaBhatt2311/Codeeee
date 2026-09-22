a5491f20f77bf751598620a3dcfaf50e4a92fa0d
PragyaBhatt2311 <pragya23bhatt11@gmail.com>
2026-09-22 22:13:02 +0530

    Time: 0 ms (100%), Space: 48.1 MB (84.49%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,21 @@
-
 class Solution {
     public int search(int[] nums, int target) {
-
+        
         int left = 0;
         int right = nums.length - 1;
 
         while (left <= right) {
-
-            int mid = (left + right) / 2;
+            
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
                 return mid;
             }
+            
             else if (nums[mid] < target) {
                 left = mid + 1;
             }
+            
             else {
                 right = mid - 1;
             }
@@ -23,3 +24,4 @@ class Solution {
         return -1;
     }
 }
+
